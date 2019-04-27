e05d2bffbfa58e8a0b8f5c97a141c38373c7f4e1
Dale Visser <dale.visser@live.com>
2019-04-27 10:32:48 -0400

    Ignore webpack config in S3 sync.

--- sync-s3.sh
+++ sync-s3.sh
@@ -4,6 +4,7 @@ CDN_ID=E1JB5T7J476T64
 echo "Syncing any changes to $S3_BUCKET"
 aws s3 sync . $S3_BUCKET --exclude=".gitignore" --exclude="*.sh" --exclude=".git/*" \
     --exclude="*.md" --exclude="node_modules/*" --exclude="src/*" --exclude="*.xcf" \
-    --exclude="package.json" --exclude="package-lock.json" --exclude="workbox-config.js"
+    --exclude="package.json" --exclude="package-lock.json" --exclude="workbox-config.js" \
+    --exclude="webpack.config.js"
 echo "Remember to invalidate any changed files so browsers get latest:"
 echo "  > aws cloudfront create-invalidation --distribution-id $CDN_ID --paths [...]"
