33898810352b275162922f88490f7d540ae194a8
Dale Visser <dale.visser@live.com>
2019-07-23 08:17:12 -0400

    Removed Bootstrap Modal to save a little bandwidth.

--- webpack.config.js
+++ webpack.config.js
@@ -44,7 +44,7 @@ module.exports = {
       Carousel: "exports-loader?Carousel!bootstrap/js/dist/carousel",
       Collapse: "exports-loader?Collapse!bootstrap/js/dist/collapse",
       Dropdown: "exports-loader?Dropdown!bootstrap/js/dist/dropdown",
-      Modal: "exports-loader?Modal!bootstrap/js/dist/modal",
+      // Modal: "exports-loader?Modal!bootstrap/js/dist/modal",
       Popover: "exports-loader?Popover!bootstrap/js/dist/popover",
       // Scrollspy: "exports-loader?Scrollspy!bootstrap/js/dist/scrollspy",
       Tab: "exports-loader?Tab!bootstrap/js/dist/tab",
