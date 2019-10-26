6009bce5fb8bd87d8bdc45df4a9116fbbfca4626
Dale Visser <dale.visser@live.com>
2019-10-25 20:26:27 -0400

    Made sync/deploy script much less likely to unnecessarily re-upload all files.

--- sync-s3.sh
+++ sync-s3.sh
@@ -1,11 +1,20 @@
 #!/usr/bin/env bash
 S3_BUCKET=s3://aws-website-knoxcclbeta-e4hkg/
 CDN_ID=E1JB5T7J476T64
-echo "Syncing any updates in dist to $S3_BUCKET"
+echo "Syncing assets that rarely change, based on size only."
 echo
-cd cd `git rev-parse --show-toplevel`
-aws s3 sync dist $S3_BUCKET
-echo "You may want to publicize significant site changes. See the README."
+cd `git rev-parse --show-toplevel`
+aws s3 sync dist $S3_BUCKET --size-only --exclude "*" --include "agendas/*" \
+    --include "images/*" --include "newsletters/*" --include "photos/*" --include "*.woff*" \
+    --include "*.eot" --include "*.svg" --include "*.jpg" --include "*.ttf"
+echo
+echo "Syncing assets that are more likely to change, considering timestamps, too."
+echo "S3 syncing is rough, and may upload some files that didn't change."
+echo
+aws s3 sync dist $S3_BUCKET --exclude "agendas/*" --exclude "images/*" \
+    --exclude "newsletters/*" --exclude "photos/*" --exclude "*.woff*" --exclude "*.eot" \
+    --exclude "*.svg" --exclude "*.jpg" --exclude "*.ttf"
 echo
 echo "Remember to invalidate any changed files so browsers get latest:"
 echo "  > aws cloudfront create-invalidation --distribution-id $CDN_ID --paths [...]"
+echo "You may also want to publicize significant site changes. See the README."
