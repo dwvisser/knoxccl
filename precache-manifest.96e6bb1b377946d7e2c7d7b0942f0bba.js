7dd80c855e058298c7093af62a15ece488fb337b
dwvisser <dwvisser@users.noreply.github.com>
2019-11-15 14:30:38 +0000

    Automated deployment: Fri Nov 15 14:30:38 UTC 2019 fbe8a19f8905fd8bbb0bc3867c7aaac211cee06b

---
+++ precache-manifest.96e6bb1b377946d7e2c7d7b0942f0bba.js
@@ -0,0 +1,365 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "url": "2ef8ba3410dcc71578a880e7064acd7a.woff"
+  },
+  {
+    "url": "418dad87601f9c8abd0e5798c0dc1feb.woff2"
+  },
+  {
+    "url": "46e7cec623d8bd790d9fdbc8de2d3ee7.eot"
+  },
+  {
+    "url": "49279363201ed19a840796e05a74a91b.svg"
+  },
+  {
+    "url": "5bf145531213545e03ff41cd27df7d2b.svg"
+  },
+  {
+    "url": "5c68fef924f1824da2c291b02f71838f.svg"
+  },
+  {
+    "url": "5e2f92123d241cabecf0b289b9b08d4a.woff2"
+  },
+  {
+    "url": "70a356610e972a6d7b6aae1b60b37707.jpg"
+  },
+  {
+    "url": "974b514f72ff3a0fc393a4b2ae047dd1.png"
+  },
+  {
+    "url": "98b6db59be947f563350d2284fc9ea36.ttf"
+  },
+  {
+    "url": "99ed7b4356f5e1ce26c94d2ea91036c4.jpg"
+  },
+  {
+    "url": "9e435847b2aaa201095455c6edcb2e11.png"
+  },
+  {
+    "url": "a350f80a4977638f0d6d69c964d87454.jpg"
+  },
+  {
+    "url": "a7140145ebaaf5fb14e40430af5d25c4.woff"
+  },
+  {
+    "url": "a7b95dbdd87e0c809570affaf366a434.eot"
+  },
+  {
+    "revision": "acb50620f4346beafad00764dded9767",
+    "url": "about.html"
+  },
+  {
+    "revision": "e0a151da3153d836d314749fc08d8ed9",
+    "url": "agendas/2017-10.html"
+  },
+  {
+    "revision": "cc23dacde14703e1cf112190ac024cdd",
+    "url": "agendas/2017-11.html"
+  },
+  {
+    "revision": "1db106b820ad540dc9a89a63545bbefc",
+    "url": "agendas/2017-11.md"
+  },
+  {
+    "revision": "29357e0cd4adc695c81cd43d470002e9",
+    "url": "agendas/2018-01.pdf"
+  },
+  {
+    "revision": "a35003fe81a7e672c194",
+    "url": "bundle.js"
+  },
+  {
+    "revision": "f632a420cdb92ed8dff4af3952893e00",
+    "url": "calendar.html"
+  },
+  {
+    "url": "ea4238faf3cbc59ea6e0a4e3b27939ae.svg"
+  },
+  {
+    "url": "ff8d9f8adb0d09f11d4816a152672f53.ttf"
+  },
+  {
+    "revision": "39b9ffeaec7715348d7a3ce278d2d7df",
+    "url": "google0415419d4f72884c.html"
+  },
+  {
+    "revision": "652c7d6aa1606fdb91f56c86816bb31a",
+    "url": "home.html"
+  },
+  {
+    "revision": "7f745f234e553718aad5d42be5ec3735",
+    "url": "images/120px-Human-emblem-web-green-128.png"
+  },
+  {
+    "revision": "20f49a4448a000fbcd2f2c97477a3743",
+    "url": "images/HorizontalWhite.png"
+  },
+  {
+    "revision": "db421e4e00ce1d7a097f670dfea24bec",
+    "url": "images/Human-emblem-web-green-144.png"
+  },
+  {
+    "revision": "f7c70c7e0625a6bfb6835c36e99e750a",
+    "url": "images/Human-emblem-web-green-192.png"
+  },
+  {
+    "revision": "80bc0fd1bedb9dcd37972b4606a042f0",
+    "url": "images/Human-emblem-web-green-48.png"
+  },
+  {
+    "revision": "090da18aa88a010204bb5035b7f7266e",
+    "url": "images/Human-emblem-web-green-512.png"
+  },
+  {
+    "revision": "af15e05699e9511e5d6590163805df92",
+    "url": "images/Human-emblem-web-green-72.png"
+  },
+  {
+    "revision": "906cd366d6b2c2ea7c404776d25059c2",
+    "url": "images/Human-emblem-web-green-80.png"
+  },
+  {
+    "revision": "0b8876903f7998f0dc1a7ac35fcd0be3",
+    "url": "images/Human-emblem-web-green-96.png"
+  },
+  {
+    "revision": "5c3c8621866091d2f6886c082491d50c",
+    "url": "images/SquareLogoBW-01-inverse.png"
+  },
+  {
+    "revision": "f274ba68d604e2f09ae55640f5c721b3",
+    "url": "images/ccl-logo.png"
+  },
+  {
+    "revision": "a88f0d280c7a4e10a7eeaf38aa2ce099",
+    "url": "images/ccl-logo.svg"
+  },
+  {
+    "revision": "71ef2c82e68246f6e83b9e56aac9e05f",
+    "url": "images/sing-for-the-climate.jpg"
+  },
+  {
+    "revision": "16498701bb0c15abaee7d71c94fce084",
+    "url": "images/smoky-mountains-1379552973wwG.jpg"
+  },
+  {
+    "revision": "e24f8bd6fefe481c67e9691747576251",
+    "url": "images/thumbnail.jpg"
+  },
+  {
+    "revision": "1222c9c0b52919af703aca345d5cc900",
+    "url": "index.html"
+  },
+  {
+    "revision": "691158d1fabe18444d0fadbf371e2918",
+    "url": "manifest.json"
+  },
+  {
+    "revision": "b81dc9eccaaef6b6ca1da9ec7dcf4c85",
+    "url": "meetings.html"
+  },
+  {
+    "revision": "a300968aa2ba95a485647a7f5528688b",
+    "url": "newsletters.html"
+  },
+  {
+    "revision": "894f923c4bef5ee56d9fd6e757bb85b7",
+    "url": "newsletters/2017-10.html"
+  },
+  {
+    "revision": "e82ee9943f9bafc6497922eca13da2c2",
+    "url": "newsletters/2017-11.html"
+  },
+  {
+    "revision": "a764b1ab50e8aa0d335088e770a83ed5",
+    "url": "photos.html"
+  },
+  {
+    "revision": "7460c5fc83a51d8ffda19d7ef6a309f0",
+    "url": "photos/20190209_100614.JPG"
+  },
+  {
+    "revision": "4f7363b206a436b7af48f0eb21df4bdb",
+    "url": "photos/20190209_102435.JPG"
+  },
+  {
+    "revision": "5cdf72d726527c9f3509b9964712ebff",
+    "url": "photos/20190209_120143.JPG"
+  },
+  {
+    "revision": "038af4e30862399fe6172962c189e352",
+    "url": "photos/20190220-Burchett-Meeting.jpeg"
+  },
+  {
+    "revision": "7a01659ca0d48c9580de9e7d3c7bab2c",
+    "url": "photos/20190729_195401.jpg"
+  },
+  {
+    "revision": "4b07ca9b892f93166125e4ba807b94c9",
+    "url": "photos/20190729_195406.jpeg"
+  },
+  {
+    "revision": "b01c82dd5e9b3067b6ad6b2ccaa00378",
+    "url": "photos/668w/20190209_100614.JPG"
+  },
+  {
+    "revision": "066b418876b679491695e0300768225a",
+    "url": "photos/668w/20190209_102435.JPG"
+  },
+  {
+    "revision": "d2b8592028c5c67bf24b534b9a1bd851",
+    "url": "photos/668w/20190209_120143.JPG"
+  },
+  {
+    "revision": "bbaad12ba39e21b0269b5de065506dc4",
+    "url": "photos/668w/20190220-Burchett-Meeting.jpeg"
+  },
+  {
+    "revision": "d70e181837b2d0d83ca8b59e463ee44a",
+    "url": "photos/668w/20190729_195401.jpg"
+  },
+  {
+    "revision": "5f28c08d344c04efde75c9fd6653f085",
+    "url": "photos/668w/20190729_195406.jpeg"
+  },
+  {
+    "revision": "3d36953323d6981c395e1d893e371bfd",
+    "url": "photos/668w/IMG_20190209_102230.jpg"
+  },
+  {
+    "revision": "829e3580f274a5f3a569a3ff7059f1eb",
+    "url": "photos/668w/IMG_20190209_110705.jpg"
+  },
+  {
+    "revision": "df4c91b2ca8157a72c3a7811bc1bf30c",
+    "url": "photos/668w/IMG_20190209_120130324.jpg"
+  },
+  {
+    "revision": "8daf2da92edb0e4def2aeccf8ddcbef4",
+    "url": "photos/668w/IMG_20190209_131412.jpg"
+  },
+  {
+    "revision": "4b1ea90bd78371e38c5a333e7dc3dba7",
+    "url": "photos/668w/IMG_20190209_131431.jpg"
+  },
+  {
+    "revision": "e30af65742a8a7dffbbea90df094bdf2",
+    "url": "photos/668w/sing2019/20190629_134650.JPG"
+  },
+  {
+    "revision": "bf21fe9ade139a24f52aea4cd28d8163",
+    "url": "photos/668w/sing2019/20190629_141856.JPG"
+  },
+  {
+    "revision": "ae68e5e2c2ba63877bbfb427e2398a40",
+    "url": "photos/668w/sing2019/20190629_142035.JPG"
+  },
+  {
+    "revision": "b0192765977bb608d9e3086473bdf552",
+    "url": "photos/668w/sing2019/20190629_145120.JPG"
+  },
+  {
+    "revision": "dd9fdfb6ca3c5e7c819659f8bb2f89d4",
+    "url": "photos/668w/sing2019/_DSC9357-Pano lt.jpg"
+  },
+  {
+    "revision": "7a3b46c9e7a61757683540d6d569fb06",
+    "url": "photos/668w/sing2019/_DSC9455.JPG"
+  },
+  {
+    "revision": "47c14cd2c193bb9cce25c3d20887c836",
+    "url": "photos/668w/sing2019/_DSC9468.JPG"
+  },
+  {
+    "revision": "77a6324fc79c440d3774db5597febbf8",
+    "url": "photos/668w/sing2019/_DSC9491-Pano.JPG"
+  },
+  {
+    "revision": "afac8a8bcb6ff726cf9496ed6c592b60",
+    "url": "photos/668w/sing2019/_DSC9494.JPG"
+  },
+  {
+    "revision": "25d9cdebb83dccb5df18602abf670b64",
+    "url": "photos/668w/sing2019/_DSC9504_2.JPG"
+  },
+  {
+    "revision": "8caa3653e68da8bc391ab56421675960",
+    "url": "photos/668w/sing2019/_DSC9508-Pano.jpg"
+  },
+  {
+    "revision": "73ae42de4d029ede6a9ebde9dbe9e1b8",
+    "url": "photos/668w/sing2019/_DSC9926.JPG"
+  },
+  {
+    "revision": "101bb6d15c23b00a6fc5dca8ac27d5de",
+    "url": "photos/IMG_20190209_102230.jpg"
+  },
+  {
+    "revision": "4fcde03b63f6a465be977bb06c3d30ef",
+    "url": "photos/IMG_20190209_110705.jpg"
+  },
+  {
+    "revision": "3b84d808cf22d4761d7359cf09ea77a4",
+    "url": "photos/IMG_20190209_120130324.jpg"
+  },
+  {
+    "revision": "f67ac44f1e0be757c7cd69cc475e070d",
+    "url": "photos/IMG_20190209_131412.jpg"
+  },
+  {
+    "revision": "3d6cff7bc52f2e6b55b7ea5eb1704a45",
+    "url": "photos/IMG_20190209_131431.jpg"
+  },
+  {
+    "revision": "ada8389a5968231cbca2b90146b75696",
+    "url": "photos/IMG_3955.jpg"
+  },
+  {
+    "revision": "f83adc22c28c9133fb9724e8b4059c85",
+    "url": "photos/sing2019/20190629_134650.JPG"
+  },
+  {
+    "revision": "defcd655f43a25703a6f4d4db365da0c",
+    "url": "photos/sing2019/20190629_141856.JPG"
+  },
+  {
+    "revision": "c7454e558b7f604dabcf1cf50cc96a55",
+    "url": "photos/sing2019/20190629_142035.JPG"
+  },
+  {
+    "revision": "fbc16179467beb5a8019668fd801485e",
+    "url": "photos/sing2019/20190629_145120.JPG"
+  },
+  {
+    "revision": "14023d62a9476262b94eb699b5516d9c",
+    "url": "photos/sing2019/_DSC9357-Pano lt.jpg"
+  },
+  {
+    "revision": "ee5ff2cb48fae2fd17c992900463e4f1",
+    "url": "photos/sing2019/_DSC9455.JPG"
+  },
+  {
+    "revision": "68f5dbfffbd54b7cc329b9a84a03a740",
+    "url": "photos/sing2019/_DSC9468.JPG"
+  },
+  {
+    "revision": "d9b02d7fcf79ef2b8891ad6715373aa1",
+    "url": "photos/sing2019/_DSC9491-Pano.JPG"
+  },
+  {
+    "revision": "c44fbc45288a9e259f4d35b17d725895",
+    "url": "photos/sing2019/_DSC9494.JPG"
+  },
+  {
+    "revision": "f4b0cf1dfe8f45a7a1c412f192a0e129",
+    "url": "photos/sing2019/_DSC9504_2.JPG"
+  },
+  {
+    "revision": "d439b46ba51a521be6bf7f37751ae23e",
+    "url": "photos/sing2019/_DSC9508-Pano.jpg"
+  },
+  {
+    "revision": "03f9c1f360305c1edeb877c8d7301b74",
+    "url": "photos/sing2019/_DSC9926.JPG"
+  }
+]);
---
+++ service-worker.js
@@ -0,0 +1,32 @@
+/**
+ * Welcome to your Workbox-powered service worker!
+ *
+ * You'll need to register this file in your web app and you should
+ * disable HTTP caching for this file too.
+ * See https://goo.gl/nhQhGp
+ *
+ * The rest of the code is auto-generated. Please don't update this file
+ * directly; instead, make changes to your Workbox build configuration
+ * and re-run your build process.
+ * See https://goo.gl/2aRDsh
+ */
+
+importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
+
+importScripts(
+  "precache-manifest.96e6bb1b377946d7e2c7d7b0942f0bba.js"
+);
+
+workbox.core.skipWaiting();
+
+workbox.core.clientsClaim();
+
+/**
+ * The workboxSW.precacheAndRoute() method efficiently caches and responds to
+ * requests for URLs in the manifest.
+ * See https://goo.gl/S9QRab
+ */
+self.__precacheManifest = [].concat(self.__precacheManifest || []);
+workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
+
+workbox.routing.registerRoute(/\.(?:png|jpg|jpeg|svg|pdf)$/i, new workbox.strategies.CacheFirst({ "cacheName":"images-and-docs", plugins: [new workbox.expiration.Plugin({ maxEntries: 100, purgeOnQuotaError: false })] }), 'GET');
