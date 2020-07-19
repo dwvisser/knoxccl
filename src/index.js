fece07141447bdaac2cf47c52b70fd0730f0379e
Dale Visser <dale.visser@live.com>
2020-07-19 14:32:32 -0400

    More third Tuesday refactoring.

--- src/index.js
+++ src/index.js
@@ -84,34 +84,32 @@ $(function() {
     });
   }
 
-  function nextThirdTuesday() {
-    const now = new Date(Date.now());
-    const nowMonth = now.getMonth();
-    const nowYear = now.getFullYear();
-    const thisMonthThirdTuesday = thirdTuesday(nowYear, nowMonth);
-    if (now <= thisMonthThirdTuesday) {
-      return thisMonthThirdTuesday;
-    }
-    const before_december = nowMonth < 11;
-    const nextYear = before_december ? nowYear : nowYear + 1;
-    const nextMonth = before_december ? nowMonth + 1 : 1;
-    return thirdTuesday(nextYear, nextMonth);
+  function firstTuesdayDayOfMonth(year, month) {
+    const tuesday = 2;  // Date uses Sunday = 0 .. Saturday == 6
+    const first_of_month_weekday = new Date(year, month, 1).getDay();
+    const delta = tuesday - first_of_month_weekday;
+    const offset = delta >= 0 ? 1 : 6;
+    return Math.sign(delta) * delta + offset;
   }
 
   function thirdTuesday(year, month) {
     return new Date(year, month, firstTuesdayDayOfMonth(year, month) + 14)
   }
 
-  function firstTuesdayDayOfMonth(year, month) {
-    const tuesday = 2;  // Date uses Sunday = 0 .. Saturday == 6
-    const first_of_month_weekday = new Date(year, month, 1).getDay();
-    if (first_of_month_weekday == tuesday) {
-      return 1;
-    }
-    if (first_of_month_weekday < tuesday) {
-      return 1 + tuesday - first_of_month_weekday;
-    }
-    return 6 + first_of_month_weekday - tuesday;
+  function nextThirdTuesday() {
+    const now = new Date(Date.now());
+    const nowMonth = now.getMonth();
+    const nowYear = now.getFullYear();
+    const thisMonthThirdTuesday = thirdTuesday(nowYear, nowMonth);
+    return now <= thisMonthThirdTuesday ? thisMonthThirdTuesday :
+      nextMonthThirdTuesday(nowYear, nowMonth);
+  }
+
+  function nextMonthThirdTuesday(nowYear, nowMonth) {
+    const before_december = nowMonth < 11;
+    const nextYear = before_december ? nowYear : nowYear + 1;
+    const nextMonth = before_december ? nowMonth + 1 : 1;
+    return thirdTuesday(nextYear, nextMonth);
   }
 
   setupAboveFoldContent();
