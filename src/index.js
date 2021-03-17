f2b5c2342bd7f74fbf057a37ecea2914f95d6538
Dale Visser <dale.visser@live.com>
2021-03-17 09:05:50 -0400

    Using my new async understanding, optimized usage of async and await for quickest visual load.

--- src/index.js
+++ src/index.js
@@ -23,49 +23,36 @@ $(function() {
 
   async function loadMeetingsTab() {
     await load("meetings", "meetings.html");
-    return Promise.all([
-      load("agenda-2017-10", "agendas/2017-10.html"),
-      load("agenda-2017-11", "agendas/2017-11.html")
-    ]);
-  }
-
-  async function loadNewslettersTab() {
-    await load("newsletters", "newsletters.html");
-    return Promise.all([
-      load("newsletter-2017-10", "newsletters/2017-10.html"),
-      load("newsletter-2017-11", "newsletters/2017-11.html")
-    ]);
+    load("agenda-2017-10", "agendas/2017-10.html");
+    load("agenda-2017-11", "agendas/2017-11.html");
   }
 
   async function loadNewslettersAndMediaTabs() {
-    await Promise.all([loadNewslettersTab(), load("media", "media.html")]);
-    lazyload();
-  }
-
-  function logError(e) {
-    console.error(e);
+    // First, load the only files with <img class=lazyload/>
+    await Promise.all([load("newsletters", "newsletters.html"),
+                       load("media", "media.html")]);
+    lazyload();  // Lazy Load, activate!
+    // also, some old content without images
+    load("newsletter-2017-10", "newsletters/2017-10.html");
+    load("newsletter-2017-11", "newsletters/2017-11.html");
   }
 
   async function setupAboveFoldContent() {
-    try {
-      setUpDarkModeToggle();
-      await load("home", "home.html").catch(logError);
-      const options = {year: "numeric", month: "long", day: "numeric"};
-      const t3 = tuesdays.nextThirdTuesday().toLocaleDateString('en-US', options);
-      $('#next-meeting-date').text(t3);
-      $("body").on("click", ".switch", function() {
-        const match = $(this)
-          .attr("class")
-          .match(/to-tab-(\w+)/);
-        if (match !== null && match.length > 1) {
-          const tab = match[1];
-          $('.nav-tabs a[href="#' + tab + '"]').tab("show");
-        }
-      });
-      await loadAboutSection();
-    } catch (e) {
-      logError(e);
-    }
+    setUpDarkModeToggle();
+    await load("home", "home.html");
+    const options = {year: "numeric", month: "long", day: "numeric"};
+    const t3 = tuesdays.nextThirdTuesday().toLocaleDateString('en-US', options);
+    $('#next-meeting-date').text(t3);
+    $("body").on("click", ".switch", function() {
+      const match = $(this)
+        .attr("class")
+        .match(/to-tab-(\w+)/);
+      if (match !== null && match.length > 1) {
+        const tab = match[1];
+        $('.nav-tabs a[href="#' + tab + '"]').tab("show");
+      }
+    });
+    await loadAboutSection();
   }
 
   function setUpDarkModeToggle() {
@@ -95,9 +82,21 @@ $(function() {
       $('#bs-version').text($.fn.tab.Constructor.VERSION);
   }
 
-  setupAboveFoldContent();
-  load("calendar", "calendar.html").catch(logError);
-  loadMeetingsTab().catch(logError);
-  loadNewslettersAndMediaTabs().catch(logError);
-  load("social", "social.html").catch(logError);
+  async function loadRestOfTabs() {
+    load("calendar", "calendar.html");
+    loadMeetingsTab();
+    loadNewslettersAndMediaTabs();
+    load("social", "social.html");
+  }
+
+  async function loadAllContent() {
+    try {
+      await setupAboveFoldContent();  // finish visible content load first
+      loadRestOfTabs();
+    } catch (e) {
+      console.error(e);
+    }
+  }
+
+  loadAllContent();
 });
